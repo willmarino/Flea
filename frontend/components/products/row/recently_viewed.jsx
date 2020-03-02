a28a10bfc94ada994fb4400189aedcb0b990fe7d
Will <w.marino997@gmail.com>
2020-03-01 23:57:01 -0500

    refactoring search filtering and recently viewed products component filtering

--- frontend/components/products/row/recently_viewed.jsx
+++ frontend/components/products/row/recently_viewed.jsx
@@ -12,7 +12,7 @@ class RecentlyViewed extends React.Component{
     debugger;
     let { products } = this.props;
     let productsArr = [];
-    for(let i = 0; i < products.length; i++){
+    for(let i = 0; i < (this.props.limit ? this.props.limit : products.length); i++){
       let p = products[i];
       productsArr.push(
         <IndexItem loggedIn={this.props.loggedIn} product={p} type="simple" key={p.id}/>
