6066e35484f4843ed4ae4c95c3aa576b3a9a394c
Will <w.marino997@gmail.com>
2020-05-27 23:26:10 -0400

    product index styling

--- frontend/components/products/row/product_row.jsx
+++ frontend/components/products/row/product_row.jsx
@@ -12,12 +12,12 @@ class ProductRow extends React.Component{
             return <IndexItem product={p} type={type} loggedIn={loggedIn} key={p.id}/>
         });
         return(
-            <div>
-                <div>
-                    <p >{category}</p>
-                    <p >See More</p>
+            <div className="product-row-container">
+                <div className="p-row-subheader">
+                    <p>{category}</p>
+                    <p className="h-light-gray" id="p-row-see-more">See More</p>
                 </div>
-                <ul>
+                <ul className="p-row">
                     {productsList}
                 </ul>
             </div>
