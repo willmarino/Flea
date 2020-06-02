9541ca4f57c35b19f0d1d4e9dec729e1d7ea385a
Will <w.marino997@gmail.com>
2020-06-02 17:59:35 -0400

    responsive css for front page

--- frontend/components/products/row/product_row.jsx
+++ frontend/components/products/row/product_row.jsx
@@ -9,19 +9,32 @@ class ProductRow extends React.Component{
 
     render(){
         let { rowOfProducts, type, loggedIn, category } = this.props;
-        let productsList = rowOfProducts.map((product) => {
-            // return <IndexItem product={p} type={type} loggedIn={loggedIn} key={p.id}/>
-            return <ProductPreview previewType={type} product={product}/>
+        // let productsList = rowOfProducts.map((product) => {
+        //     // return <IndexItem product={p} type={type} loggedIn={loggedIn} key={p.id}/>
+        //     return <ProductPreview previewType={type} product={product}/>
+        // });
+        let firstHalfProductList = [];
+        let secondHalfProductList = [];
+        rowOfProducts.forEach((product, i) => {
+            if(i < (rowOfProducts.length / 2)){
+                firstHalfProductList.push(<ProductPreview previewType={type} product={product}/>);
+            }else{
+                secondHalfProductList.push(<ProductPreview previewType={type} product={product}/>);
+            }
         });
+        firstHalfProductList = <ul className="p-row-half">{firstHalfProductList}</ul>
+        secondHalfProductList = <ul className="p-row-half">{secondHalfProductList}</ul>
         return(
             <div className="product-row-container">
                 <div className="p-row-subheader">
                     <p>{category}</p>
                     <p className="h-light-gray" id="p-row-see-more">See More</p>
                 </div>
-                <ul className="p-row">
-                    {productsList}
-                </ul>
+                <div className="p-row">
+                    {/* {productsList} */}
+                    {firstHalfProductList}
+                    {secondHalfProductList}
+                </div>
             </div>
         );
     }
