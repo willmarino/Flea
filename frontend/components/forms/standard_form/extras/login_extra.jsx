604ce44626440f696bf87e08b2b0d217c80b8a06
Will <w.marino997@gmail.com>
2020-06-06 23:54:50 -0400

    adding extra option for standard form body component used in login modal

---
+++ frontend/components/forms/standard_form/extras/login_extra.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoginExtra = (props) => {
+  return(
+    <div>
+      <div>
+        <input type="checkbox"/>
+        <p>Stay Signed In</p>
+      </div>
+      <p>Forgot Password?</p>
+    </div>
+  )
+}
+
+export default LoginExtra;
