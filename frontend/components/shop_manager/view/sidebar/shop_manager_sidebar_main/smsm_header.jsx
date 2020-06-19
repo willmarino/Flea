b5070437a7a3ee79895b82790c06327f497b0e3a
Will <w.marino997@gmail.com>
2020-06-18 23:39:41 -0400

    shop manager modal rework

--- frontend/components/shop_manager/view/sidebar/shop_manager_sidebar_main/smsm_header.jsx
+++ frontend/components/shop_manager/view/sidebar/shop_manager_sidebar_main/smsm_header.jsx
@@ -3,15 +3,24 @@ import SMSMHeaderModal from './smsm_header_modal';
 import { GiShop } from 'react-icons/gi';
 import { BsArrowDownShort } from 'react-icons/bs';
 
-const SMSMHeader = () => {
-  return(
-    <div className='SMSM-header'>
-      <GiShop/>
-      <p>Shop Manager</p>
-      <BsArrowDownShort/>
-      <SMSMHeaderModal/>
-    </div>
-  )
+class SMSMHeader extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      modalShowing: false
+    }
+  }
+  render(){
+    let modal = (this.state.modalShowing) ? <SMSMHeaderModal/> : null;
+    return(
+      <div className='SMSM-header' onClick={() => {this.setState({modalShowing: !this.state.modalShowing})}}>
+        <GiShop/>
+        <p>Shop Manager</p>
+        <BsArrowDownShort/>
+        {modal}
+      </div>
+    )
+  }
 }
 
 export default SMSMHeader;
