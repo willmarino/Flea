e83544b2ec7820f52d84094be4a0f55a4bbe10c8
Will <w.marino997@gmail.com>
2020-06-19 23:36:51 -0400

    shop management

--- frontend/components/shop_manager/view/sidebar/shop_manager_sidebar_main/smsm_header_modal.jsx
+++ frontend/components/shop_manager/view/sidebar/shop_manager_sidebar_main/smsm_header_modal.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { GiShop } from 'react-icons/gi';
 
-const SMSMHeaderModal = () => {
+const SMSMHeaderModal = (props) => {
+  let { history } = props;
   return(
     <div className="SMSM-header-modal">
       <div className='SMSM-header-modal-row' id="SMSMHM-top-row">
@@ -10,11 +11,11 @@ const SMSMHeaderModal = () => {
       </div>
       <div onClick={() => { history.push('/shops-manager/dashboard') }} className='SMSM-header-modal-row'>
         <GiShop/>
-        <p>Shop Manager</p>
+        <p className="SMSM-header-modal-row-message">Shop Manager</p>
       </div>
       <div onClick={() => { history.push('/') }} className='SMSM-header-modal-row'>
         <div className='makeshift-etsy-icon'><p>E</p></div>
-        <p>Etsy</p>
+        <p className="SMSM-header-modal-row-message">Etsy</p>
       </div>
     </div>
   )
